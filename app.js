f71deeaf90b140f22e89ed10b85e98b168f819a1
manavmody <manavmody0007@gmail.com>
2022-02-01 21:52:10 +0530

    add gitignore , procfiles and update ports

--- app.js
+++ app.js
@@ -168,6 +168,12 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
